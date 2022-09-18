17c431d438e21712c2913df22c245bdf7f9c989e
= <anwar988805@yahoo.com>
2022-09-18 15:20:26 +0000

    Setup the Home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CrowdByte</title>
+    <title>CrowdByte - Learn With Confidence</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="/img/favicon.ico">
 </head>
 
-<body>
-    @yield('content')
+<body class="text-white">
+    <div class="w-[90vw] m-auto py-[20px] main">
+        <div class="flex flex-row justify-between border-1 py-4">
+            <div class="items-center m1-2 flex justify-center">
+                <a href="#">
+                    <img src="/img/logo.png" height="20px" width="200" />
+                </a>
+            </div>
+            <nav class="border-0 top-menu">
+                <ul class="flex flex-row">
+                    <li>
+                        <a href="#">html</a>
+                    </li>
+                    <li>
+                        <a href="#">css</a>
+                    </li>
+                    <li>
+                        <a href='#'>javascript</a>
+                    </li>
+                    <li>
+                        <a href='#'>node</a>
+                    </li>
+                    <li>
+                        <a href='#'>javascript</a>
+                    </li>
+                    <li>
+                        <a href='#'>php</a>
+                    </li>
+                    <li>
+                        <a href='#'>mysql</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="border-0 mr-[0px] flex justify-center justify-around">
+                {{-- <button class="bg-white text-red-600 px-4 rounded-md mr-2">Login</button> --}}
+                <button class="bg-red-600 px-[30px] text-black">Subscribe</button>
+            </div>
+        </div>
+
+        {{-- Body section --}}
+        <div class="mt-[80px] border-0 justify-center heading">
+            <div class="w-[60vw] border-0">
+                <h1 class="text-[5rem] font-extrabold leading-[4.2rem] text-center text-gray-200">No need to feel lost
+                    when learning
+                    to
+                    code.
+                </h1>
+            </div>
+            <div class="w-[50vw]">
+                <h3 class="mt-[30px] text-center text-[1.8rem] text-gray-400 w-[50vw] leading-[2.5rem]">
+                    You can do it just like veryone else. Build your confidence by puting your work out to the world.
+                </h3>
+            </div>
+            <div></div>
+        </div>
+    </div>
 </body>
 
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,11 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Welcome To CrowdByte</title>
 </head>
 
 <body>
+    <div class="bg-green-900 h-[100vw] w-[80vw]">
+        <div>
+            <div>Logo</div>
+            <nav>
+                <ul>
+                    <li>
+                        <a>About</a>
+                    </li>
+                    <li>
+                        <a>Product</a>
+                    </li>
+                    <li>
+                        <a>Blog</a>
+                    </li>
+                </ul>
+            </nav>
 
+            <div class="w-100">
+                Login
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
